2d1597eb3ae04fe95dbdf93705a0397a894fe5be
Shriram <alekhshriram@gmail.com>
2025-07-29 15:22:58 -0500

    script

---
+++ rotate_ref.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import argparse
+
+import sys
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--ref', type = str, required=True)
+parser.add_argument('--pos', type = int, required = True)
+args = parser.parse_args()
+
+ref_fasta = args.ref
+rotate_pos = args.pos
+
+char_count = 0
+new_str = '' # new string that's gonna be your rotated circular genome
+header = ''
+
+f = open(ref_fasta, 'r')
+for i in f:
+    i = i.strip()
+    if i == '':
+        continue
+    if i[0] == '>':
+        header = i
+    else:
+        new_str += i
+f.close()
+
+left = new_str[:rotate_pos]
+right = new_str[rotate_pos:]
+
+print(header+" test rotate", file = sys.stderr)
+
+print("{}{}", right, left, file = sys.stderr)
+
+
+final_str = right+left
+sys.stdout.write(header+" rotated\n")
+sys.stdout.write(final_str)
